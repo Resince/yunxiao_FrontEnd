b9f332d4dc5c6a7f24865e9224c9bffcad5e74ca
Resince <1876702860@qq.com>
2023-09-08 09:48:57 +0800

    添加compressor.js

--- src/pages/UploadPic/UploadSide.jsx
+++ src/pages/UploadPic/UploadSide.jsx
@@ -1,21 +1,15 @@
 import React from "react";
 import { Button } from "antd";
 import { useStore } from "../../store";
-import http from "../../utils/axios";
+import { uploadImg } from "../../service/uploadImages";
+
 
 const UploadSide = () => {
     const store = useStore();
 
     const handleOnClick = () => {
-        const formdata = new FormData();
-        store.UploadImgStore.getImgList.map((item) => {
-            formdata.append("image", item);
-        });
-        http.post("/dev/nvoice/reg", formdata).then((ans) => {
-            console.log(ans);
-        });
-        http.post("/dev/nvoice/reg", formdata).then((ans) => {
-            console.log(ans);
+        uploadImg(store.UploadImgStore.getImgList.at(0)).then((res) => {
+            console.log(res);
         });
     };
 
---
+++ src/service/uploadImages.js
@@ -0,0 +1,23 @@
+import http from "../utils/axios";
+
+export function uploadImgList(imgList) {
+
+    const formdata = new FormData();
+    imgList.map(item => {
+        formdata.append("file", item);
+    })
+    const res = http.post("dev/nvoice/reg", formdata);
+    return res;
+}
+
+export function uploadImg(img) {
+    const formdata = new FormData();
+    formdata.append("file", img)
+    return http.post("dev/invoice/reg", formdata);
+}
+
+export function uploadImgLocal(img) {
+    const formdata = new FormData();
+    formdata.append("file", img)
+    return http.post("local/upload", formdata);
+}
